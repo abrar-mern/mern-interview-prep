ff442f2d55bdbf53ce1bf41ebc7407d7cbdcfc2b
Abrar Babar Khan <mrabrarbabarkhan@gmail.com>
2026-05-22 00:46:00 +0530

    higher order functions

---
+++ Javascript/functions/higherorder/higherorder.js
@@ -0,0 +1,57 @@
+// Calculate area of a circle
+
+const radius = [3,2,1,4];
+const calculateArea = function(radius){
+    const output = [];
+    for (let i=0;i<radius.length;i++){
+        output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+}
+
+// console.log(calculateArea(radius))  
+
+
+const calculateCircumference = function(radius){
+    const output = [];
+    for(let i=0;i<radius.length;i++){
+        output.push(2 * Math.PI * radius[i])
+    }
+    return output
+}
+// console.log(calculateCircumference(radius))
+
+
+const calculateDiameter = function(radius){
+    const output = [];
+    for (let i=0; i<radius.length; i++){
+        output.push(2 * radius[i]);
+    }
+    return output
+}
+// console.log(calculateDiameter(radius))
+
+// Dynamic Code using DRY principle
+
+const area = function(radius){
+    return Math.PI * radius * radius
+}
+
+const circumference = function(radius){
+    return 2 * Math.PI * radius
+}
+
+const diameter = function(radius){
+    return 2  * radius
+}
+
+const calculate = function(radius,logic){
+    const output = [];
+    for(let i=0; i<radius.length; i++){
+        output.push(logic(radius[i]));
+    }
+    return output;
+}
+console.log(calculate(radius,area))
+console.log(calculate(radius,circumference))
+console.log(calculate(radius,diameter))
---
+++ Javascript/functions/higherorder/higherorder.md
@@ -0,0 +1,4 @@
+1.) What are higher order function ?
+Answer - A function which take another function as an argument or return a function as an output.
+
+-------------------------------------------------------------------------------------------
