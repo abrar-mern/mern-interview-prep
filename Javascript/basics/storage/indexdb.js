16d7a980c5a1de0b13e94a200b1034fd83ebdae5
Abrar Babar Khan <mrabrarbabarkhan@gmail.com>
2026-05-26 01:35:59 +0530

    indexdb concept

--- Javascript/basics/storage/indexdb.js
+++ Javascript/basics/storage/indexdb.js
@@ -1,30 +1,45 @@
 let openRequest = indexedDB.open('collegeDB',1);
 console.log(openRequest);
 
-// Onsuccess method
 openRequest.onsuccess = (e) => {
-    console.log('Form Success')
-}
-
-// onupgradeneeded method
-openRequest.onupgradeneeded = (e) => {
-    console.log('Upgrade Needed')
+    console.log('On Success');
     let db = openRequest.result;
-    if(!db.objectStoreNames.contains('students')){
-        let request = db.createObjectStore("students",{keyPath: 'id'});
-        request.createIndex("name", "name", {unique:false});
-        request.createIndex("email", "email", {unique:true});
+    let transaction = db.transaction("students","readwrite");
+    let storeObject = transaction.objectStore("students");
+
+    // Add Values
+    // let request = storeObject.put({
+    //     id : "3",
+    //     name : "Rahul",
+    //     email : "rahul@gmail.com"
+    // })
+
+    // Get Values
+    // let request = storeObject.getAll(IDBKeyRange.bound("2","3"))
+
+    // Delete 
+    let request = storeObject.delete("3");
+
+
+    request.onsuccess = (e) => {
+        console.log(e.target.result)
+    }
+
+    request.onerror = (e) => {
+        console.log(e)
     }
 }
 
-// onerror method
-openRequest.onerror = (e) => {
-    console.log('Error Successfully', e)
+openRequest.onupgradeneeded = (e) => {
+    console.log('On Upgrade');
+    let db = openRequest.result;
+    if(!db.objectStoreNames.contains("students")){
+        let studentStore = db.createObjectStore("students",{keyPath:"id"});
+        studentStore.createIndex("name","name",{unique:false})
+        studentStore.createIndex("email","email",{unique:true})
+    }
 }
 
-
-// Notes : -
-
-// 1. We have to open the database using .open()
-
-// 2. We will assign the object - result to openRequest
+openRequest.onerror = (e) => {
+    console.log('Error',e);
+}
