567f76f739dc90c8902eb19275670de7649bce9a
Abrar Babar Khan <mrabrarbabarkhan@gmail.com>
2026-05-28 21:35:44 +0530

    getcookie functionality

--- Javascript/basics/cookies/cookies.js
+++ Javascript/basics/cookies/cookies.js
@@ -2,12 +2,14 @@ document.cookie = 'name=AbrarKhan; expires = Friday 30 May 2026 12:0:00 UTC; pat
 document.cookie = 'lastName=Babulkidua; expires = Friday 30 May 2026 12:0:00 UTC; path=./'
 
 
+
 // const date = new Date();
 // console.log(date);
 // console.log(typeof date);
 
 setCookie("email","abc@gmail.com",3);
 
+// Create Cookie
 function setCookie(name,value,daystoexpire){
     const date = new Date();
     date.setTime(date.getTime() + (daystoexpire * 24 * 60 * 60 * 1000));
@@ -17,14 +19,31 @@ function setCookie(name,value,daystoexpire){
 
 setCookie("gaon","delhi",2);
 
-
+// Delete Cookie
 function deleteCookie(name){
    const looseWatch = new Date();
    looseWatch.setTime(0);
    document.cookie = `${name}=;expires = ${looseWatch.toUTCString()};`
 }
 deleteCookie("email");
+
 console.log(document.cookie);
 
 
-
+// Get Cookie
+// getCookie("email")
+function getCookie(name){
+    const cookieName = name + "=";
+    const cookieArray = document.cookie.split(';');
+    for (let i=0; i<cookieArray.length;i++){
+        let cookie = cookieArray[i];
+        cookie = cookie.trim();
+        if(cookie.indexOf(cookieName)==0){
+            return cookie.substring(cookieName.length,cookie.length)
+        }
+    }
+    return null;
+}
+console.log("We are invoking our get method here")
+console.log(getCookie("name"));
+// console.log(document.cookie);
---
+++ Javascript/basics/cookies/datemethods.js
@@ -0,0 +1,16 @@
+const date = new Date()
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+const hour = date.getHours();
+const minute = date.getMinutes();
+const second = date.getSeconds();
+
+console.log(date);
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minute);
+console.log(second);
+
