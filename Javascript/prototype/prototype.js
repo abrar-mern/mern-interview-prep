cd0ecb29c577e2b2c0209c6264a386371592d9ef
Abrar Babar Khan <mrabrarbabarkhan@gmail.com>
2026-06-14 23:49:52 +0530

    prototype

--- Javascript/prototype/prototype.js
+++ Javascript/prototype/prototype.js
@@ -1,24 +1,6 @@
-function multipleBy5(num){
+function multiple(num){
     return num * 5
 }
-multipleBy5.power = 2
-const output = multipleBy5(5)
-// console.log(output)
-// console.log(multipleBy5.power)
-
-function createUser(name,score){
-    this.name = name;
-    this.score = score;
-}
-
-createUser.prototype.increment = function (){
-   this.score++
-}
-
-createUser.prototype.printMe = function(){
-    console.log(`Price is ${this.score}`)
-}
-
-const chai = new createUser('tea',23);
-const tea = new createUser('coffee',30)
-// chai.printMe();
+console.log(multiple(2))
+console.log(multiple.power=2)
+console.log(multiple.prototype)
--- Javascript/prototype/prototype.md
+++ Javascript/prototype/prototype.md
@@ -3,7 +3,7 @@ Answer - If we want to add properties at later stage to a function which can be 
 
 Concept of  how prototype works
 
-Note : - When we are creating object or function or variables for variables it wrapped under object and attached to hidden object called __proto__ to our object.
+Note : - When we are creating object or function or variables it wrapped under object and attached to hidden object called __proto__ to our object.
 
 let arr = ["Abrar","Atish","Pranali"]
 
@@ -15,3 +15,4 @@ b.) Array Master
 c.) Function Master
 
 and after that arr.__proto.__proto__ = Object.prototype
+
