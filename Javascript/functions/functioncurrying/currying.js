d8fc10c171df64150b6ac87992501f637fc08e1b
Abrar Babar Khan <mrabrarbabarkhan@gmail.com>
2026-06-17 15:23:14 +0530

    function currying

--- Javascript/functions/functioncurrying/currying.js
+++ Javascript/functions/functioncurrying/currying.js
@@ -8,7 +8,7 @@ function carriedSum(a){
 // Invoke a function
 let step1 = carriedSum(20)
 let result = step1(25);
-console.log(`Output from first method : ${result}`);
+// console.log(`Output from first method : ${result}`);
 
 
 // 2nd Method
@@ -19,4 +19,34 @@ function carriedSum1(a){
 }
 
 let result1 = carriedSum1(20)(25);
-console.log(`Output from second method : ${result1}`);
+// console.log(`Output from second method : ${result1}`);
+
+
+///////////////
+
+let multiply = function(x,y){
+    console.log(x * y)
+}
+
+let threeMultiple = multiply.bind(this,3) 
+threeMultiple(2)
+
+let fiveMultiple = multiply.bind(this,5)
+fiveMultiple(3)
+
+
+// 1st Method
+
+function newCarriedSum(a){
+    return function newSum(b){
+        return a + b
+    }
+}
+
+let sum = newCarriedSum(2);
+let resultnew = sum(10)
+
+// 2nd Method
+
+let output = newCarriedSum(40)(40)
+console.log(output)
